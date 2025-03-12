36a5693d8e0f529b1891a684971c71a6775223ec
Josh Taylor <theofficialjoshuataylor@gmail.com>
2025-03-11 18:30:52 -0700

    Changed to patch

--- models/ir_ui_view.py
+++ models/ir_ui_view.py
@@ -3,4 +3,7 @@ from odoo import models
 class IrUIView(models.Model):
     _inherit = 'ir.ui.view'
 
-    _type_selection = models.Model._type_selection + [('grid', 'Grid')]
+    @classmethod
+    def _build_model(cls, registry):
+        super()._build_model(registry)
+        cls._patch_method('type_selection', lambda self: super(IrUIView, self).type_selection() + [('grid', 'Grid')])
